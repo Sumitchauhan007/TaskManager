90f3908c4a7dec5367b527c75b12dd5d44885b92
Sumit Chauhan <127190593+Sumitchauhan007@users.noreply.github.com>
2025-07-18 15:36:36 +0530

    controller change

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const {protect,adminOnly} = require("../middlewares/authMiddleware");
+const { getDashboardData, getTasks, getTaskById, createTask, updateTask, deleteTask, updateTaskStatus, updateTaskChecklist, getUserDashboardData } = require("../controllers/taskController");
 
-const router = express.Routes();
+const router = express.Router();
 
 //task management routes
 router.get("/dashboard-data", protect,getDashboardData);
-router.get("/user-dashboard-data", protect,getDashboardData);
+router.get("/user-dashboard-data", protect,getUserDashboardData);
 router.get("/", protect,getTasks);
 router.get("/:id", protect,getTaskById);
 router.post("/", protect,adminOnly,createTask);
@@ -15,4 +16,4 @@ router.put("/:id/status" ,protect,updateTaskStatus);
 router.put("/:id/todo" ,protect,updateTaskChecklist);
 
 
-module.export = router;
+module.exports = router;
