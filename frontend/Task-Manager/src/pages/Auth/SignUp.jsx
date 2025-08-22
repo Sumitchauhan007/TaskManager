689c931b93af0b4d7ae08816d1b81548284efa84
Sumit Chauhan <127190593+Sumitchauhan007@users.noreply.github.com>
2025-08-22 22:04:07 +0530

    SignUp

--- frontend/Task-Manager/src/pages/Auth/SignUp.jsx
+++ frontend/Task-Manager/src/pages/Auth/SignUp.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
 import { validateEmail } from '../../utils/helper';
 import ProfilePhotoSelector from '../../components/Inputs/ProfilePhotoSelector';
+import Input from '../../components/inputs/Input';
+import { Link,useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
   const [profilePic, setProfilePic] = useState(null);
@@ -44,15 +46,43 @@ const SignUp = () => {
         </p>
 
         <form onSubmit={handleSignUp}>
-          <ProfilePhotoSelector image={profilrPic} setImage={setProfilePic}/>
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic}/>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <Input value={fullName}
             onChange={({ target }) => setFullName(target.value)}
             label="Full Name"
             placeholder="John Wick"
-            type="text"/>
+            type="text"
+            />
+            <Input value={email}
+          onChange={({ target}) => setEmail(target.value)}
+          label="Email"
+          placeholder="john@example.com"
+          type="text"/>
+
+          <Input value={password}
+          onChange={({ target}) => setPassword(target.value)}
+          label="Password"
+          placeholder="Min 6 characters"
+          type="password"/>
+
+          <Input value={password}
+          onChange={({ target}) => setPassword(target.value)}
+          label="Admin Invite Token"
+          placeholder="6 Digit Code"
+          type="text"/>
           </div>
+
+           {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+
+          <button type='submit' className='btn-primary'>SIGN UP</button>
+          <p className='text-[13px] text-slate-800 mt-3'>
+             Already have an account?{" "}
+            <Link className='font-medium text-primary underline' to="/login">
+            Login
+            </Link>
+          </p>
         </form>
       </div>
     </AuthLayout>
