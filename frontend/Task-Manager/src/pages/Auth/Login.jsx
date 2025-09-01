e4b516e4e7e4b8e89f7bbfedcae259a36e21f27e
Sumit Chauhan <127190593+Sumitchauhan007@users.noreply.github.com>
2025-09-01 23:18:39 +0530

    da

--- frontend/Task-Manager/src/pages/Auth/Login.jsx
+++ frontend/Task-Manager/src/pages/Auth/Login.jsx
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import AuthLayout from '../../components/layouts/AuthLayout';
 import { Link, useNavigate } from 'react-router-dom';
 import Input from '../../components/inputs/Input';
 import { validateEmail } from '../../utils/helper';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
+import { UserContext } from '../../context/userContext';
 
 const Login = () => {
   const [email,setEmail] = useState("");
   const [password,setPassword] = useState("");
   const [error,setError] = useState("");
    
+  const {updateUser} = useContext(UserContext)
   const navigate = useNavigate();
 
   //handle login form submit
@@ -39,7 +41,7 @@ const Login = () => {
 
      if(token) {
       localStorage.setItem("token" , token);
-      updateUser()//comment
+      updateUser(response.data)
 
       //redirect based on role
       if (role === "admin") {
--- frontend/Task-Manager/src/pages/Auth/dashbaord.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function dashbaord() {
-  return (
-    <div>dashbaord</div>
-  )
-}
-
-export default dashbaord
