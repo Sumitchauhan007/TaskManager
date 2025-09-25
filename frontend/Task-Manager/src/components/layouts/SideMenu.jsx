c027ffe68fb2497412b21e954ce1c59efb00db31
Sumit Chauhan <127190593+Sumitchauhan007@users.noreply.github.com>
2025-09-25 23:46:25 +0530

    a

--- frontend/Task-Manager/src/components/layouts/SideMenu.jsx
+++ frontend/Task-Manager/src/components/layouts/SideMenu.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 const SideMenu = ({ activeMenu }) => {
-    const {user ,clearUser} = useContext(UserContext);
-    const [SideMenuData, setSideMenuData] = useState([
+  const { user, clearUser } = useContext(UserContext);
+  const [SideMenuData, setSideMenuData] = useState([
   return <div>SideMenu</div>;
-  
-}; //comment
+
+};
 
 export default SideMenu;
