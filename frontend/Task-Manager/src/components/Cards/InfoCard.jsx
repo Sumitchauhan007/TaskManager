84174b337cdcd106ef2aaec635db3c31bca885f3
Sumit Chauhan <127190593+Sumitchauhan007@users.noreply.github.com>
2025-10-04 22:40:31 +0530

    removed comment

--- frontend/Task-Manager/src/components/Cards/InfoCard.jsx
+++ frontend/Task-Manager/src/components/Cards/InfoCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const InfoCard = ({icon, label, value, color}) => {
+const InfoCard = ({ icon, label, value, color }) => {
   return (
     <div>InfoCard
       <div className={`p-4 rounded-lg shadow-md flex items-center gap-4 ${color}`}>
@@ -11,7 +11,7 @@ const InfoCard = ({icon, label, value, color}) => {
           <p className='text-sm text-white'>{label}</p>
           <h3 className='text-xl md:text-2xl font-semibold text-white mt-1.5'>{value}</h3>
 
-          asdfghjkl;
+
         </div>
       </div>
     </div>
