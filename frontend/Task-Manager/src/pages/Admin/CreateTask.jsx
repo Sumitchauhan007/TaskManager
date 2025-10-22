9751e1777d6d9514a7e7013a22e02b25aab0eb53
Sumit Chauhan <127190593+Sumitchauhan007@users.noreply.github.com>
2025-10-23 00:10:25 +0530

    create task

--- frontend/Task-Manager/src/pages/Admin/CreateTask.jsx
+++ frontend/Task-Manager/src/pages/Admin/CreateTask.jsx
@@ -68,17 +68,56 @@ const CreateTask = () => {
 
   return (
     <DashboardLayout activeMenu="Create Task">
-    <div className="">
-      <div className="">
-        <div className="">
-          <div className="">
-            <h2 className=''>
-              {taskId}
-            </h2>
+    <div className="mt-5">
+      <div className="grid grid-cols-1 md:grid-cols-4 mt-4">
+        <div className="form-card col-span-3">
+          <div className="flex items-center justify-between">
+            <h2 className='text-xl md:text-xl font-medium'>
+              {taskId ? "Update Task" : "Create New Task"}
+              </h2>
+
+              {taskId && (
+                <button
+                 className='flex items-center gap-1.5 text-[13px] font-medium text-rose-500 bg-rose-50 rounded px-2 py-1 border border-rose-100 hover:border-rose-300 cursor-pointer'
+                 onClick={() => setOpenDeleteAlert(true)}
+                 >
+                  <LuTrash2 className='text-base' /> Delete
+                 </button>
+              )}
+              </div>
+
+            <div className='mt-4'>
+              <label className='text-xs font-medium text-slate-600'>
+                Task Title
+              </label>
+
+              <input
+               placeholder='Create App UI'
+               className='form-input'
+               value={taskData.title}
+               onChange={({ target}) =>
+              handleValueChange("title", target.value)
+              }
+              />
+              </div>
+              <div className="mt-3">
+                <label className='text-xs font-medium text-slate-600'>
+                  Description
+                  </label>
+
+                  <textarea
+                   rows={4}
+                   placeholder='Task Description'
+                   className='form-input'
+                   value={taskData.description}
+                   onChange={({ target}) =>
+                    handleValueChange("description", target.value)
+                   }
+                  />
+              </div>
           </div>
         </div>
       </div>
-    </div>
     </DashboardLayout>
   )
 }
