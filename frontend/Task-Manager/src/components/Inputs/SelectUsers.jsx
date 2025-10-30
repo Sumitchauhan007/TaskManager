bb26c9b2f0a7c42dcc69a677907a93e9616338b0
Sumit Chauhan <127190593+Sumitchauhan007@users.noreply.github.com>
2025-10-30 22:13:37 +0530

    selecteuser

---
+++ frontend/Task-Manager/src/components/Inputs/SelectUsers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SelectUsers({
+    selectedUsers,
+    setSelectedUsers
+}) {
+  return (
+    <div>SelectUsers</div>
+  )
+}
+
+export default SelectUsers
--- frontend/Task-Manager/src/pages/Admin/CreateTask.jsx
+++ frontend/Task-Manager/src/pages/Admin/CreateTask.jsx
@@ -8,6 +8,7 @@ import {useLoaction, useNavigate} from "react-router-dom";
 import moment from "moment";
 import { LuTrash2 } from 'react-icons/lu';
 import SelectDropdown from '../../components/Inputs/SelectDropdown';
+import SelectUsers from '../../components/Inputs/SelectUsers';
 
 const CreateTask = () => {
    
@@ -128,6 +129,35 @@ const CreateTask = () => {
                   placeholder="Select Priority"
                   />
                 </div>
+
+                <div className="col-span-6 md:col-span-4">
+                  <label className="text-xs font-medium text-slate-600">
+                    Due Date
+                  </label>
+
+                  <input
+                    placeholder="Create App UI"
+                    className="form-input"
+                    value={taskData.dueDate}
+                    onChange={({ target }) =>
+                      handleValueChange("dueDate", target.value)
+                    }
+                    type="date"
+                  />
+                </div>
+
+                <div className="col-span-12 md:col-span-3">
+                  <label className="text-xs font-medium text-slate-600">
+                    Assign To
+                  </label>
+                  <SelectUsers
+                    selectedUsers={taskData.assignedTo}
+                  setSelectedUsers={(value) => {
+                  handleValueChange("assignedTo", value);
+                  }}
+                  />
+                </div>
+
               </div>
           </div>
         </div>
