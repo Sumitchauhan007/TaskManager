e7aea6d8ea159380e000bc392d53ad7f381e6eac
Sumit Chauhan <127190593+Sumitchauhan007@users.noreply.github.com>
2025-11-02 21:27:48 +0530

    modal

--- frontend/Task-Manager/src/components/Inputs/SelectUsers.jsx
+++ frontend/Task-Manager/src/components/Inputs/SelectUsers.jsx
@@ -1,14 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { LuUsers } from 'react-icons/lu';
+import Modal from '../layouts/Modal';
 
 function SelectUsers({
     selectedUsers,
     setSelectedUsers
 }) {
+  const [allUsers, setAllUsers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [tempSelectedUsers, setTempSelectedUsers] = useState([]);
+
+  const getAllUsers = async () => {
+    try{
+      const response = await axiosInstance.get(API_PATHS.USERS.GET_ALL_USERS);
+      if (response.data?.length > 0) {
+        setAllUsers(response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  const toggleUserSelection = (userId) => {
+    setTempSelectedUsers((prev) => 
+    prev.includes(userId)
+  ? prev.filter((id) => id !== userId)
+  : [...prev, userId]
+  );
+  };
+
+  const handleAssign = () => {
+    setSelectedUsers(tempSelectedUsers);
+    setIsModalOpen(false);
+  };
+
+  const selectedUserAvatars = allUsers
+    .filter((user) => selectedUsers.includes(user.id))
+    .map((user) => user.profileImageUrl);
+
+    useEffect(() => {
+      getAllUsers();
+    }, []);
+
+    useEffect(() => {
+      if (selectedUser.length === 0) {
+        setTempSelectedUsers([]);
+      }
+
+      return () => {};
+    }, [selectedUsers]);
+    
   return (
-    <div>SelectUsers</div>
-  )
-}
+  <div className='space-y-4 mt-2'>
+    {selectedUserAvatars.length === 0 && (
+      <button 
+    className='card-btn' onClick={() => setIsModalOpen(true)}>
+      <LuUsers className="text-sm" /> Add Members
+    </button> 
+    )}
 
-export default SelectUsers
+    <Modal 
+    isOpen={isModalOpen}
+    onClose={() => setIsModalOpen(false)}
+    title="Select Users"
+    >
+      <div className="space-y-4 h-[60vh] overflow-y-auto"></div>
+      </Modal>
+  </div>
+  );
+};
 
-<select name="" id=""></select>
+export default SelectUsers;   
---
+++ frontend/Task-Manager/src/components/layouts/Modal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Modal = ({children, isOpen, onClose, title}) => {
+ if (!isOpen) return;
+
+ return <div className=""></div>
+};
+
+export default Modal
